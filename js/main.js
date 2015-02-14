be6a086f360d1d2e0237b9b19ba17c96489f9ebb
Wael Showair <show0017@algonquinlive.com>
2015-02-14 01:22:37 -0500

    remove anchor-link class and menu-toggle id, use correct way to load svg icon using snap svg

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", function(){
 
     /* Load hamburger svg icon that represents menu icon to html. */
-    var snapCanvas = Snap( 64, 64 );
-    snapCanvas.attr( 'viewBox', '0 0 64 64' );
 	var svgElement = document.getElementById("svg-hamburger");
-    svgElement.appendChild( snapCanvas.node );
+    var snapCanvas = Snap( svgElement );
+    snapCanvas.attr( 'viewBox', '0 0 64 64' );
+    //svgElement.appendChild( snapCanvas.node );
     Snap.load( "svg/hamburger.svg", function (f) {
 			var g = f.select( 'g' );
 			snapCanvas.append( g );
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function(){
     /* Execute hamburger menu animation. */
     window.scrollTo(0, 1);
 
-    var menuToggle = document.querySelector("#menu-toggle");
+    var menuToggle = document.querySelector("#svg-hamburger");
     menuToggle.addEventListener("click", function(e){
         document.body.classList.toggle("active");
         e.preventDefault();
