c238a98269763012604b528701903d7189231d6b
Wael Showair <show0017@algonquinlive.com>
2015-02-14 02:17:52 -0500

    animate hamburger svg icon using snap svg library upon clicking

--- js/main.js
+++ js/main.js
@@ -1,23 +1,54 @@
+var svgIcon = (function(){
+
+    var toggled = false;
+    var snapCanvas;
+    var load = function(svgId, svgUrl){
+        /* Load hamburger svg icon that represents menu icon to html. */
+	var svgElement = document.getElementById(svgId);
+    snapCanvas = Snap( svgElement );
+    snapCanvas.attr( 'viewBox', '0 0 64 64' );
+    Snap.load( svgUrl, function (fragment) {
+			var g = fragment.select( 'g' );
+			snapCanvas.append( g );
+    });
+    /* Execute hamburger icon animation. */
+    svgElement.addEventListener("click", toggle);
+    }
+
+    var toggle = function(){
+        for( var i = 0; i < hamburgerCross.animation.length; i++ ) {
+			var a = hamburgerCross.animation[ i ];
+            var el = snapCanvas.select( a.el );
+            var animProp = toggled ? a.animProperties.from :
+                        a.animProperties.to;
+            var val = animProp.val;
+
+			if( animProp.before ) {
+				el.attr( JSON.parse( animProp.before ) );
+			}
+            el.attr( JSON.parse( val ) );
+        }/*  for loop*/
+        toggled = !toggled;
+    }
+
+    return{
+        load: load
+    }
+})();
+
 document.addEventListener("DOMContentLoaded", function(){
 
-    /* Load hamburger svg icon that represents menu icon to html. */
-	var svgElement = document.getElementById("svg-hamburger");
-    var snapCanvas = Snap( svgElement );
-    snapCanvas.attr( 'viewBox', '0 0 64 64' );
-    //svgElement.appendChild( snapCanvas.node );
-    Snap.load( "svg/hamburger.svg", function (f) {
-			var g = f.select( 'g' );
-			snapCanvas.append( g );
+    svgIcon.load("svg-hamburger", hamburgerCross.url);
 
-
-    });
-
-    /* Execute hamburger menu animation. */
+    /* Execute HTML animatin of hamburger menu.
+    Note that hamburger menu has two listeners for the click event:
+    1. Handler for HTML animation to push the page from left to right.
+    2. Handler for svg icon animation to change it from 3 staked lines to x
+    */
     window.scrollTo(0, 1);
-
-    var menuToggle = document.querySelector("#svg-hamburger");
-    menuToggle.addEventListener("click", function(e){
-        document.body.classList.toggle("active");
+    var hamburgerMenu = document.querySelector("#svg-hamburger");
+    hamburgerMenu.addEventListener("click", function(e){
         e.preventDefault();
+        document.body.classList.toggle("active");
     })
 });
