2cfb59595ad1732b7fb4953796746230967d6303
Wael Showair <show0017@algonquinlive.com>
2015-02-14 01:17:58 -0500

    add hamburger svg menu using snap svg library

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(){
+
+    /* Load hamburger svg icon that represents menu icon to html. */
+    var snapCanvas = Snap( 64, 64 );
+    snapCanvas.attr( 'viewBox', '0 0 64 64' );
+	var svgElement = document.getElementById("svg-hamburger");
+    svgElement.appendChild( snapCanvas.node );
+    Snap.load( "svg/hamburger.svg", function (f) {
+			var g = f.select( 'g' );
+			snapCanvas.append( g );
+
+
+    });
+
+    /* Execute hamburger menu animation. */
     window.scrollTo(0, 1);
 
     var menuToggle = document.querySelector("#menu-toggle");
